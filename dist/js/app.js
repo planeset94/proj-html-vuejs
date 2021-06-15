087cd22e3db48c36c3ebcbf55cefd711f333ca54
planeset94 <planeset94@gmail.com>
2021-06-15 20:23:09 +0200

    main:section 1- top part

--- dist/js/app.js
+++ dist/js/app.js
@@ -25,6 +25,19 @@ var app = new Vue({
     }, {
       text: "blog",
       "class": ''
+    }],
+    keyIndicators: [{
+      emoticon: 'fas fa-user-friends',
+      title: 'Human Capital',
+      par: 'Humanizing business: Harness the power of technology to improve the way people work.'
+    }, {
+      emoticon: 'fas fa-chart-pie',
+      title: 'Core Business',
+      par: 'It takes innovative approaches to transform, modernize, and run existing platforms.'
+    }, {
+      emoticon: 'fas fa-tachometer-alt',
+      title: 'Performance',
+      par: 'Achieving maximum impact and value from investments in finance and supply chain.'
     }]
   },
   methods: {}
--- src/app.js
+++ src/app.js
@@ -30,6 +30,32 @@ const app = new Vue(
 
             ],
 
+            keyIndicators: [
+
+                {
+                    emoticon: 'fas fa-user-friends',
+                    title: 'Human Capital',
+                    par: 'Humanizing business: Harness the power of technology to improve the way people work.'
+
+                },
+                {
+                    emoticon: 'fas fa-chart-pie',
+                    title: 'Core Business',
+                    par: 'It takes innovative approaches to transform, modernize, and run existing platforms.'
+
+                },
+
+
+                {
+                    emoticon: 'fas fa-tachometer-alt',
+                    title: 'Performance',
+                    par: 'Achieving maximum impact and value from investments in finance and supply chain.'
+
+                },
+
+
+            ],
+
 
 
 
