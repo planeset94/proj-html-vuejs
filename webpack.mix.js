28fb839a1bba22cb5af0806cdb99d8c516355ba4
planeset94 <planeset94@gmail.com>
2021-06-15 13:36:11 +0200

    jumbotron: bg-image + black filter+ tcentral title

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,3 +6,6 @@ mix
     .sass('src/app.scss', 'css')
     .copyDirectory('src/img', 'dist/img')
     .setPublicPath('dist')
+    .options({
+        processCssUrls: false // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
+    })
